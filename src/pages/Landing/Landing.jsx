7a87ce6a1213ce28abd538c2747d52170eb4d9e8
im-victor-mendez <victor.mmc.pl@hotmail.com>
2024-07-17 21:28:54 -0600

    style: Styled Landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,28 +1,53 @@
-import './Landing.scss'
+import './Landing.scss';
 
-import React from 'react'
-import LOGO from "../../assets/my_unsplash_logo.svg";
-import { useAuth } from "../../contexts/authContext";
+import React from 'react';
+import LOGO from '../../assets/my_unsplash_logo.svg';
+import SolutionOverview from '../../assets/SolutionOverview.png';
+import { useAuth } from '../../contexts/authContext';
 
 function Landing() {
-    const { loginWithGoogle, loginAnonymously } = useAuth()
+	const { loginWithGoogle, loginAnonymously } = useAuth();
 
-  return (
-    <section id='landing'>
-        <header>
-            <img src={LOGO} alt="My unsplash logo" />
-        </header>
+	return (
+		<section id='landing'>
+			<header>
+				<img src={LOGO} alt='My unsplash logo' />
+			</header>
 
-        <center>
-            <h1>Login with:</h1>
+			<main>
+				<h1>Login with:</h1>
 
-            <div className='login-methods'>
-                <button onClick={loginWithGoogle}>Google</button>
-                <button onClick={loginAnonymously}>Anonymously</button>
-            </div>
-        </center>
-    </section>
-  )
+				<div className='login-methods'>
+					<button onClick={loginWithGoogle}>Google</button>
+					<button onClick={loginAnonymously}>Anonymously</button>
+				</div>
+			</main>
+
+			<center>
+				<p>To use this app you need to authenticate by one of these methods.</p>
+				<img src={SolutionOverview} alt='Solution Overview' />
+
+				<h2>Auth Differences</h2>
+				<div id='auth-differences'>
+					<div>
+						<h3>Google</h3>
+						<ul>
+							<li>
+								Your photos will be saved in Firebase database linked to your
+								account.
+							</li>
+						</ul>
+					</div>
+					<div>
+						<h3>Anonymously</h3>
+						<ul>
+							<li>Your photos will not be saved.</li>
+						</ul>
+					</div>
+				</div>
+			</center>
+		</section>
+	);
 }
 
-export default Landing
+export default Landing;
