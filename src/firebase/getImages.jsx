b221a10105013db5907492141655c38bed3994d8
im-victor-mendez <victor.mmc.pl@hotmail.com>
2024-07-17 20:43:54 -0600

    refactor: Refactored Images layout

--- src/firebase/getImages.jsx
+++ src/firebase/getImages.jsx
@@ -1,34 +1,35 @@
-import { onValue, ref } from "firebase/database";
-import { useEffect, useState } from "react";
-import { useAuth } from "../contexts/authContext";
-import { db } from "./firebase";
+import { onValue, ref } from 'firebase/database';
+import { useEffect, useState } from 'react';
+import { useAuth } from '../contexts/authContext';
+import { db } from './firebase';
 
 function UseImages() {
-    const [images, setImages] = useState(null)
-    const { user } = useAuth()
+	const [images, setImages] = useState(null);
+	const { user } = useAuth();
 
-    useEffect(() => {
-        getImages()
-    }, [])
+	useEffect(() => {
+		getImages();
+	}, []);
 
-    return { images }
+	return { images };
 
-    function getImages() {
-        onValue(ref(db, `${user.uid}`), snapshot => onValueCallback(snapshot))
-    }
+	function getImages() {
+		onValue(ref(db, `${user.uid}`), snapshot => onValueCallback(snapshot));
+	}
 
-    function onValueCallback(snapshot) {
-        setImages([])
+	function onValueCallback(snapshot) {
+		setImages([]);
 
-        const data = snapshot.val()
+		const data = snapshot.val();
 
-        if (data) Object.keys(data)
-        .forEach(image => setImages(
-            oldArray => [...oldArray,
-                { uid: image, url: data[image].url, label: data[image].label }
-            ]
-        ))
-    }
+		if (data)
+			Object.keys(data).forEach(image =>
+				setImages(oldArray => [
+					...oldArray,
+					{ uid: image, url: data[image].url, label: data[image].label },
+				])
+			);
+	}
 }
 
-export default UseImages
+export default UseImages;
--- src/layouts/Images/Images.jsx
+++ src/layouts/Images/Images.jsx
@@ -11,6 +11,7 @@ function Images() {
 	const { search } = useSearch();
 	const { images } = UseImages();
 
+	// Number of image columns depending on windows width
 	const breakpointCols = { default: 3, 800: 2, 700: 1 };
 
 	return (
@@ -20,30 +21,41 @@ function Images() {
 			className='my-masonry-grid'
 			columnClassName='my-masonry-grid_column'
 		>
-			{!images ? (
-				<Loading />
-			) : images.length === 0 ? (
-				<>No images founded or</>
-			) : (
-				images
-					.filter(image => {
-						if (!search) return image;
-						if (!image.label) return;
-
-						const label = image.label.toLowerCase();
-						if (label.includes(search.toLowerCase())) return image;
-					})
-					.map(image => (
-						<Image
-							key={image.uid}
-							uid={image.uid}
-							url={image.url}
-							label={image.label}
-						/>
-					))
-			)}
+			{displayImages(images, search)}
 		</Masonry>
 	);
+}
+
+function displayImages(images, search) {
+	// Initial State
+	if (images == null) return <Loading />;
+
+	// No results on query
+	if (images.length === 0) return <>No images found</>;
+
+	// Image list applying search filter
+	return images.filter(filterImages()).map(mappingImages());
+
+	function filterImages() {
+		return image => {
+			if (!search) return image;
+			if (!image.label) return;
+
+			const label = image.label.toLowerCase();
+			if (label.includes(search.toLowerCase())) return image;
+		};
+	}
+
+	function mappingImages() {
+		return image => (
+			<Image
+				key={image.uid}
+				uid={image.uid}
+				url={image.url}
+				label={image.label}
+			/>
+		);
+	}
 }
 
 export default Images;
