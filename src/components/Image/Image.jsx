3fd580f440ad60ecf148e9448299906aa96aa56b
im-victor-mendez <victor.mmc.pl@hotmail.com>
2024-07-17 18:24:01 -0600

    refactor: Refactored Image component

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -3,18 +3,40 @@ import './Image.scss';
 import React, { useState } from 'react';
 import { useImage } from '../../contexts/imageContext';
 
+/**
+ * **Image**
+ *
+ * Component to display image in Home page.
+ * @param string uid
+ * @param string url
+ * @param string label
+ * @returns JSX.Element
+ */
 function Image({ uid, url, label }) {
 	const [image, setImage] = useState({ width: undefined, height: undefined });
 
 	const { changeImageUid } = useImage();
 
+	/**
+	 * **On Load**
+	 *
+	 * Gets natural image size and sets into image state.
+	 * @param event
+	 */
 	function onLoad(event) {
 		const target = event.target;
+
 		const width = target.naturalWidth;
 		const height = target.naturalHeight;
+
 		setImage({ width, height });
 	}
 
+	/**
+	 * **On Delete**
+	 *
+	 * Idk.
+	 */
 	function onDelete() {
 		const deleteMessage = document.getElementById('delete_image-message');
 		deleteMessage.classList.add('active');
